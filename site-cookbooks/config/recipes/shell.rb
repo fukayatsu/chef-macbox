1e33fa844f2caf46e034b41dff4e1e2b1513258a
fukayatsu <fukayatsu@gmail.com>
2013-04-11 23:36:42 +0900

    shell as root

--- site-cookbooks/config/recipes/shell.rb
+++ site-cookbooks/config/recipes/shell.rb
@@ -1,4 +1,6 @@
 bash "set zsh to default shell" do
+  user "root"
+  group "wheel"
   code <<-EOT
     _etc_shells=/etc/shells
     _zsh_path=/usr/local/bin/zsh
@@ -8,6 +10,6 @@ bash "set zsh to default shell" do
       echo _zsh_path >> $_zsh_path
     fi
 
-    chsh -s $_zsh_path
+    chsh -s $_zsh_path fukayatsu
   EOT
 end
