8264b4058d571ac76060b33a44a1fe2a9e9e9d5a
fukayatsu <fukayatsu@gmail.com>
2013-04-11 23:19:07 +0900

    add recipe[config::shell]

---
+++ site-cookbooks/config/recipes/shell.rb
@@ -0,0 +1,13 @@
+bash "set zsh to default shell" do
+  code <<-EOT
+    _etc_shells=/etc/shells
+    _zsh_path=/usr/local/bin/zsh
+    _zsh_path_in_etc_shells=$(cat $_etc_shells | grep $_zsh_path)
+
+    if [ "X"$_zsh_path_in_etc_shells = "X" ]; then
+      echo _zsh_path >> $_zsh_path
+    fi
+
+    chsh -s $_zsh_path
+  EOT
+end
