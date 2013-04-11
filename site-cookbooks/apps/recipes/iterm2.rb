f9ff8d94e151eb5da084f6e182315a25d7b10c72
fukayatsu <fukayatsu@gmail.com>
2013-04-12 01:17:59 +0900

    [from now] 2013/04/12 01:17:59
    diff --git a/nodes/local.json b/nodes/local.json
    index f8d9826..97bc7f7 100644
    --- a/nodes/local.json
    +++ b/nodes/local.json
    @@ -1,5 +1,5 @@
     {
    -   "user":{
    +    "user":{
             "name": "fukayatsu",
             "group" : "staff",
             "home": "/Users/fukayatsu"
    @@ -7,16 +7,26 @@
         "run_list": [
             "recipe[homebrew]",
             "recipe[dmg]",
    +        "recipe[zip_app]",
             "recipe[mac_os_x]",
             "recipe[brew]",
             "recipe[apps::ccmenu]",
             "recipe[apps::chrome]",
             "recipe[apps::dropbox]",
    +        "recipe[apps::iterm2]",
             "recipe[apps::sublime_text_2]",
             "recipe[config::dock]",
             "recipe[config::shell]",
             "recipe[dotfiles]"
         ],
    +    "zip_app":{
    +        "apps": [
    +            {
    +                "name"        : "iTerm",
    +                "source"      : "https://iterm2.googlecode.com/files/iTerm2_v1_0_0.zip"
    +            }
    +        ]
    +    },
         "homebrew": {
             "packages": [
                 "bvi",
    diff --git a/site-cookbooks/apps/recipes/iterm2.rb b/site-cookbooks/apps/recipes/iterm2.rb
    index 947bd16..1e79581 100644
    --- a/site-cookbooks/apps/recipes/iterm2.rb
    +++ b/site-cookbooks/apps/recipes/iterm2.rb
    @@ -1,6 +1,6 @@
     include_recipe "zip_app"
    
    -zip_app_package "Divvy" do
    -  source  "https://iterm2.googlecode.com/files/iTerm2_v1_0_0.zip"
    -end
    +# zip_app_package "iTerm2" do
    +#   source  "https://iterm2.googlecode.com/files/iTerm2_v1_0_0.zip"
    +# end

--- site-cookbooks/apps/recipes/iterm2.rb
+++ site-cookbooks/apps/recipes/iterm2.rb
@@ -1,6 +1,6 @@
 include_recipe "zip_app"
 
-zip_app_package "Divvy" do
-  source  "https://iterm2.googlecode.com/files/iTerm2_v1_0_0.zip"
-end
+# zip_app_package "iTerm2" do
+#   source  "https://iterm2.googlecode.com/files/iTerm2_v1_0_0.zip"
+# end
 
